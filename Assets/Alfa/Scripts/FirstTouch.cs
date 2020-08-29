e7207b22075a06e121b9ba5b4a2221647905f2c3
Odney do Nascimento <odney.nascimento@gmail.com>
2020-08-29 12:48:58 -0300

    UMA primeiros passos

---
+++ Assets/Alfa/Scripts/FirstTouch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTouch : MonoBehaviour
+{
+
+    private static Dictionary<int, Touch> toutchStore = new Dictionary<int, Touch>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0) {
+
+            for(int tcIdx = 0; tcIdx < Input.touches.Length; tcIdx++) {
+                Touch touch = Input.touches[tcIdx];
+                switch (touch.phase) {
+                    case TouchPhase.Began:
+                        toutchStore[tcIdx] = touch;
+                        break;
+                    case TouchPhase.Ended:
+                    case TouchPhase.Canceled:
+                        toutchStore.Remove(tcIdx);
+                        break;
+                    case TouchPhase.Moved:
+                        if (toutchStore.ContainsKey(tcIdx)) {
+                            // DO MOVE/ TURN
+                        }
+                        break;
+                }
+            }
+
+        }
+    }
+}
